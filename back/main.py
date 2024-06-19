9ea37124673847ae1e95973d9bb39aaf51d837a7
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-06-18 22:37:22 -0600

    integracion de back repuesta de API openAI funcional en respuesta

--- back/main.py
+++ back/main.py
@@ -5,17 +5,22 @@ from openai import OpenAI
 app = Flask(__name__)
 CORS(app)  # Permitir solicitudes CORS desde cualquier origen
 
+def read_text_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
 # Configura tu cliente OpenAI
-client = OpenAI(api_key="aqui va un token")
+client = OpenAI(api_key="sk-proj-ICzHe8jbnGYQWDNzJqJ2T3BlbkFJEwjlFwtKF4Hqdp7aWdro")
 
 @app.route('/get_response')
 def get_response():
+    user_message = read_text_file('../front/files/prueba.txt')
     # Obtén la respuesta del modelo
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "responde unicamente con un si o con un no"},
-            {"role": "user", "content": "los arboles son plantas"}
+            {"role": "user", "content": user_message}
         ]
     )
     content = completion.choices[0].message.content
