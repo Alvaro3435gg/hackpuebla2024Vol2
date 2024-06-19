6afddc45f5db5e6d92296f3a214781b7c5f0232e
Daniel Flores Rojas <danyflorojas@gmail.com>
2024-06-19 13:20:23 -0600

    modo obscuro

--- front/js/popup.js
+++ front/js/popup.js
@@ -1,7 +1,7 @@
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const query = document.getElementById('search').value; // Cambiado de 'query' a 'search'
+    const query = document.getElementById('search').value;
     const filetype = document.getElementById('filetype').value;
     const source = document.getElementById('source').value;
 
@@ -14,20 +14,15 @@ document.getElementById('form').addEventListener('submit', function(event) {
         optimizedSearch += ` site:edu`;
         searchUrl = `https://www.google.com/search?q=${encodeURIComponent(optimizedSearch)}`;
     } else if (source === 'scholar') {
-        // Google Scholar no soporta filetype:
         optimizedSearch += ` -controversy`;
         searchUrl = `https://scholar.google.com/scholar?q=${encodeURIComponent(optimizedSearch)}`;
     } else if (source === 'pubmed') {
-        // PubMed tampoco soporta filetype: ni la mayoría de los operadores de búsqueda avanzada
         searchUrl = `https://pubmed.ncbi.nlm.nih.gov/?term=${encodeURIComponent(query)}`;
     } else if (source === 'jstor') {
-        // JSTOR no soporta operadores como filetype:
         searchUrl = `https://www.jstor.org/action/doBasicSearch?Query=${encodeURIComponent(query)}`;
     } else if (source === 'IEEE_Xplore') {
-        // IEEE Xplore no soporta operadores como filetype:
         searchUrl = `https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=${encodeURIComponent(query)}`;
-    }  else if (source === 'Web_of_Science') {
-        // Web of Science no soporta operadores como filetype:
+    } else if (source === 'Web_of_Science') {
         searchUrl = `https://www.webofscience.com/wos/woscc/summary/=${encodeURIComponent(query)}`;
     } else if (source === 'wikipedia') {
         // Wikipedia search
@@ -36,4 +31,8 @@ document.getElementById('form').addEventListener('submit', function(event) {
 
     chrome.tabs.create({ url: searchUrl });
     document.getElementById('optimizedSearch').innerText = `Optimized Search: ${optimizedSearch}`;
+});
+
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
 });
